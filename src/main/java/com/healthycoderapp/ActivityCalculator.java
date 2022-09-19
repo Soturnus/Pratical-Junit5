57777cd1cc1e248551b49b3b177c5d45643bcc78
Soturnus <rivaldo.valhalla@gmail.com>
2022-09-19 14:37:21 -0300

    ActivityCalculator created using TDD pratice

---
+++ src/main/java/com/healthycoderapp/ActivityCalculator.java
@@ -0,0 +1,24 @@
+package com.healthycoderapp;
+
+public class ActivityCalculator {
+	
+	private static final int WORKOUT_DURATION_MIN = 45;
+	
+	public static String rateActivityLevel(int weeklyCardionMin, int weeklyWorkoutSessions) {
+		
+		if (weeklyCardionMin < 0 || weeklyWorkoutSessions < 0) {
+			throw new RuntimeException("Input below 0");
+		}
+		
+		int totalMinutes = weeklyCardionMin + weeklyWorkoutSessions * WORKOUT_DURATION_MIN;
+		double avgDailyActivityMins = totalMinutes / 7.0;
+		
+		if (avgDailyActivityMins < 20) {
+			return "bad";
+		} else if (avgDailyActivityMins < 40) {
+			return "average";
+		}
+		return "good";
+	}
+
+}
